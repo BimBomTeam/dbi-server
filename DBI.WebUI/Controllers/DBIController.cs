a002f65ef7275eddf9b3e242f84853e4eb2ca191
Bartosz Arseniuk <arseniuk22864@stud.akademiabialska.pl>
2023-11-13 16:35:14 +0100

    Created DBIController with Test Method

---
+++ DBI.WebUI/Controllers/DBIController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DBI.WebUI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DBIController : ControllerBase
+    {
+        public async Task<IActionResult> Test()
+        {
+            return Ok();
+        }
+    }
+}
