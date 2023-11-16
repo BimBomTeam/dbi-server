ffde8fe3e644c7087c26e2ea551ef5f6ab27657e
Wojciech Męczyński <wojteq898@gmail.com>
2023-11-16 22:05:56 +0100

    Add correct format verification (base64) in ImageHelper

--- DBI.WebUI/Services/DogBreedIdentificationService.cs
+++ DBI.WebUI/Services/DogBreedIdentificationService.cs
@@ -1,13 +1,17 @@
-﻿using System.Buffers.Text;
-
-namespace DBI.WebUI.Services
+﻿namespace DBI.WebUI.Services
 {
     public class DogBreedIdentificationService
     {
         public string Identify(string base64)
         {
-            string scaledBase64 = ImageHelper.ScaleImage(base64);
-            return scaledBase64;
+            try
+            {
+                string scaledBase64 = ImageHelper.ScaleImage(base64);
+                return scaledBase64;
+            }catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
         }
     }
 }
--- DBI.WebUI/Services/ImageHelper.cs
+++ DBI.WebUI/Services/ImageHelper.cs
@@ -1,23 +1,35 @@
-﻿namespace DBI.WebUI.Services
+﻿public static class ImageHelper
 {
-    public static class ImageHelper
-    {
-        private const int SIZE = 300;
-        public static string ScaleImage(string base64Image)
-        {
-            
-            byte[] imageBytes = Convert.FromBase64String(base64Image);
-            using (var inputStream = new MemoryStream(imageBytes))
-            using (var image = Image.Load(inputStream))
-            {
-                image.Mutate(x => x.Resize(SIZE, SIZE));
+    private const int SIZE = 300;
 
-                using (var outputStream = new MemoryStream())
+    public static string ScaleImage(string base64Image)
+    {
+        try
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64Image);
+            string base64Check = Convert.ToBase64String(imageBytes);
+            if (base64Check.Equals(base64Image))
+            {
+                using (var inputStream = new MemoryStream(imageBytes))
+                using (var image = Image.Load(inputStream))
                 {
-                    image.SaveAsJpeg(outputStream);
-                    return Convert.ToBase64String(outputStream.ToArray());
+                    image.Mutate(x => x.Resize(SIZE, SIZE));
+
+                    using (var outputStream = new MemoryStream())
+                    {
+                        image.SaveAsJpeg(outputStream);
+                        return Convert.ToBase64String(outputStream.ToArray());
+                    }
                 }
             }
+            else
+            {
+                throw new ArgumentException("Bad format Base64.");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new ArgumentException("Bad format Base64.", ex);
         }
     }
 }
