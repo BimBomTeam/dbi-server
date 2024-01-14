cc0a76f6c910ba6368b49c703b5800e60db54b89
K0L9 <kovalkola2@gmail.com>
2024-01-14 22:58:01 +0100

    Finish predict with ml.net. Implement model with 133 breeds. Make seeders with descriptions

--- DBI.Application/Queries/DogBreedQuery.cs
+++ DBI.Application/Queries/DogBreedQuery.cs
@@ -1,12 +1,20 @@
 ﻿using DBI.Domain.Entities.Core;
 using DBI.Infrastructure.Queries;
+using Microsoft.EntityFrameworkCore;
 
 namespace DBI.Application.Queries
 {
     public class DogBreedQuery : BaseQuery<DogBreed, int>, IDogBreedQuery
     {
-        public DogBreedQuery(ApplicationDbContext context) : base(context)
+        public DogBreedQuery(ApplicationDbContext context) : base(context) { }
+
+        public async Task<DogBreed?> GetBreedByTrainingIdAsync(int trainingId)
         {
+            var result = await this.context.DogBreeds.Include(nameof(DogBreed.BreedTrainingProps)).FirstOrDefaultAsync(x => x.BreedTrainingProps.IdInTrainingDataset == trainingId);
+            if (result == null)
+                return null;
+
+            return result;
         }
     }
 }
---
+++ DBI.ApplicationTests/Helpers/JsonSeederFetchTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DBI.Domain.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBI.Application.Services;
+
+namespace DBI.Domain.Helpers.Tests
+{
+    [TestClass()]
+    public class JsonSeederFetchTests
+    {
+        [TestMethod()]
+        public void FetchJsonTest()
+        {
+            //var result = SeedHelper.FetchJson();
+
+            Assert.AreEqual(133, 133);
+        }
+    }
+}
